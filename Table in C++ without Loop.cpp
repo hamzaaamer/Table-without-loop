40b209c220c0f199a61c66175557d2e681439ce8
Hamza Aamer <50561910+hamzaaamer@users.noreply.github.com>
2023-04-19 18:20:32 +0500

    Add files via upload

---
+++ Table in C++ without Loop.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cout<<"Enter the number of the table you want to see : \n";
+    
+    cin>>a;
+    
+    cout<< a <<" x 1 = " << a*1 <<"\n";
+    
+    // cout<< a; cout<<" x 1 = "; cout<< a*1; cout<<"\n";
+    cout<< a; cout<<" x 2 = "; cout<< a*2; cout<<"\n";
+    cout<< a; cout<<" x 3 = "; cout<< a*3; cout<<"\n";
+    cout<< a; cout<<" x 4 = "; cout<< a*4; cout<<"\n";
+    cout<< a; cout<<" x 5 = "; cout<< a*5; cout<<"\n";
+    cout<< a; cout<<" x 6 = "; cout<< a*6; cout<<"\n";
+    cout<< a; cout<<" x 7 = "; cout<< a*7; cout<<"\n";
+    cout<< a; cout<<" x 8 = "; cout<< a*8; cout<<"\n";
+    cout<< a; cout<<" x 9 = "; cout<< a*9; cout<<"\n";
+    cout<< a; cout<<" x 10 ="; cout<< a*10; cout<<"\n";
+    
+
+    
+    return 0;
+
+}
+
